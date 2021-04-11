75ce5e94353f9791aa84176165f801c97b420bee
Robin Stuedle <robin.stuedle@uni-ulm.de>
2021-04-11 16:34:55 +0200

    add exports that are used in the webgraph library

--- src/endpoint.ts
+++ src/endpoint.ts
@@ -9,4 +9,9 @@ import QuadTree from "./quadtree";
 import MouseCaptor from "./captors/mouse";
 import WebGLRenderer from "./renderers/webgl";
 
-export { Camera, QuadTree, MouseCaptor, WebGLRenderer };
+import { animateNodes } from "./animate";
+import easings from "./easings";
+import { AbstractNodeProgram } from "./renderers/webgl/programs/common/node";
+import { floatColor } from "./renderers/webgl/utils";
+
+export { Camera, QuadTree, MouseCaptor, WebGLRenderer, animateNodes, easings, AbstractNodeProgram, floatColor };
